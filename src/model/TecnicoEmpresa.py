edba144f13fb8a8dd9dc592f75d0e3522112c872
cbelloexplicit <97253486+cbelloexplicit@users.noreply.github.com>
2025-10-13 19:24:48 -0300

    Update TecnicoEmpresa.py

--- src/model/TecnicoEmpresa.py
+++ src/model/TecnicoEmpresa.py
@@ -1,10 +1,25 @@
+from model.Tecnico import Tecnico
+from model.Empresa import Empresa
+
 class TecnicoEmpresa:
-    def __init__(self, ):
-        """
-        Construtor da classe.
+    def __init__(self, tecnico:Tecnico, empresa:Empresa):
+        self.__tecnico = tecnico
+        self.__empresa = empresa
 
-        Args:
-            id_tecnico (int)
-            id_empresa (int)
+    # Getters
+    def get_tecnico(self) -> Tecnico: 
+        return self.__tecnico
+    def get_empresa(self) -> Empresa: 
+        return self.__empresa
 
-        """
+    # Setters
+    def set_tecnico(self, tecnico:Tecnico): 
+        self.__tecnico = tecnico
+    def set_empresa(self, empresa:Empresa): 
+        self.__empresa = empresa
+    
+    # To String
+    def to_string(self) -> str:
+        nome_tecnico = self.get_tecnico().get_contato().get_nome_contato() if self.get_tecnico() and self.get_tecnico().get_contato() else "N/A"
+        nome_empresa = self.get_empresa().get_nome_fantasia() if self.get_empresa() else "N/A"
+        return f"Técnico: {nome_tecnico} | Empresa: {nome_empresa}"
