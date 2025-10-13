de487e432a61fe410dccdc8f816766d1e74b7fee
cbelloexplicit <97253486+cbelloexplicit@users.noreply.github.com>
2025-10-13 19:24:33 -0300

    Update Tecnico.py

--- src/model/Tecnico.py
+++ src/model/Tecnico.py
@@ -1,11 +1,28 @@
+from model.Contato import Contato
+
 class Tecnico:
-    def __init__(self, ):
-        """
-        Construtor da classe.
+    def __init__(self, id_tecnico:int, local:str, contato:Contato):
+        self.__id_tecnico = id_tecnico
+        self.__local = local
+        self.__contato = contato
 
-        Args:
-            id_tecnico (int)
-            local (str)
-            id_contato (int)
+    # Getters
+    def get_id_tecnico(self) -> int: 
+        return self.__id_tecnico
+    def get_local(self) -> str: 
+        return self.__local
+    def get_contato(self) -> Contato: 
+        return self.__contato
 
-        """
+    # Setters
+    def set_id_tecnico(self, id_tecnico:int): 
+        self.__id_tecnico = id_tecnico
+    def set_local(self, local:str): 
+        self.__local = local
+    def set_contato(self, contato:Contato): 
+        self.__contato = contato
+
+    # To String
+    def to_string(self) -> str:
+        nome = self.get_contato().get_nome_contato() if self.get_contato() else "N/A"
+        return f"ID: {self.get_id_tecnico()} | Nome: {nome} | Local: {self.get_local()}"
