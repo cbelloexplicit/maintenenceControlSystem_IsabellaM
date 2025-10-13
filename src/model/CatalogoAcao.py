9df9c63d70f931ff68a52e70613f00c813c5c6b8
Isabella Mendes <isabelamendes827@gmail.com>
2025-10-13 16:14:24 -0300

    update

--- src/model/CatalogoAcao.py
+++ src/model/CatalogoAcao.py
@@ -1,5 +1,5 @@
 class CatalogoAcao:
-    def __init__(self, ):
+    def __init__(self, id_catalogo_acao: int, codigo_acao: str, descricao_acao: str):
         """
         Construtor da classe.
 
@@ -8,3 +8,11 @@ class CatalogoAcao:
             codigo_acao (str)
             descricao_acao (str)
         """
+        self.__id_catalogo_acao = id_catalogo_acao
+        self.__codigo_acao = codigo_acao
+        self.__descricao_acao = descricao_acao
+    # getters
+
+    # setters
+
+    #to_string
