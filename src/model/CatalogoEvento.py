d90fe0656bb4e85c0e14295fd8aa253c839c88d6
cbelloexplicit <97253486+cbelloexplicit@users.noreply.github.com>
2025-10-13 19:22:36 -0300

    Update CatalogoEvento.py

--- src/model/CatalogoEvento.py
+++ src/model/CatalogoEvento.py
@@ -1,9 +1,20 @@
 class CatalogoEvento:
-    def __init__(self, ):
-        """
-        Construtor da classe.
+    def __init__(self, id_catalogo_eventos:int, codigo_evento:str):
+        self.__id_catalogo_eventos = id_catalogo_eventos
+        self.__codigo_evento = codigo_evento
 
-        Args:
-            id_catalogo_eventos (int)
-            codigo_evento (str)
-        """
+    # Getters
+    def get_id_catalogo_eventos(self) -> int: 
+        return self.__id_catalogo_eventos
+    def get_codigo_evento(self) -> str: 
+        return self.__codigo_evento
+
+    # Setters
+    def set_id_catalogo_eventos(self, id_catalogo_eventos:int): 
+        self.__id_catalogo_eventos = id_catalogo_eventos
+    def set_codigo_evento(self, codigo_evento:str): 
+        self.__codigo_evento = codigo_evento
+
+    # To String
+    def to_string(self) -> str:
+        return f"ID: {self.get_id_catalogo_eventos()} | Evento: {self.get_codigo_evento()}"
