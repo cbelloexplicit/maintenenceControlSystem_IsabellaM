d3c9b61f8bdd3cc7120e7bf1bac4bd4c1a64c0ba
cbelloexplicit <97253486+cbelloexplicit@users.noreply.github.com>
2025-10-13 19:22:06 -0300

    Update CatalogoAcao.py

--- src/model/CatalogoAcao.py
+++ src/model/CatalogoAcao.py
@@ -1,18 +1,25 @@
 class CatalogoAcao:
-    def __init__(self, id_catalogo_acao: int, codigo_acao: str, descricao_acao: str):
-        """
-        Construtor da classe.
-
-        Args:
-            id_catalogo_acao (int)
-            codigo_acao (str)
-            descricao_acao (str)
-        """
+    def __init__(self, id_catalogo_acao:int, codigo_acao:str, descricao_acao:str = None):
         self.__id_catalogo_acao = id_catalogo_acao
         self.__codigo_acao = codigo_acao
         self.__descricao_acao = descricao_acao
-    # getters
 
-    # setters
+    # Getters
+    def get_id_catalogo_acao(self) -> int: 
+        return self.__id_catalogo_acao
+    def get_codigo_acao(self) -> str: 
+        return self.__codigo_acao
+    def get_descricao_acao(self) -> str: 
+        return self.__descricao_acao
 
-    #to_string
+    # Setters
+    def set_id_catalogo_acao(self, id_catalogo_acao:int): 
+        self.__id_catalogo_acao = id_catalogo_acao
+    def set_codigo_acao(self, codigo_acao:str): 
+        self.__codigo_acao = codigo_acao
+    def set_descricao_acao(self, descricao_acao:str): 
+        self.__descricao_acao = descricao_acao
+
+    # To String
+    def to_string(self) -> str:
+        return f"[{self.get_codigo_acao()}] {self.get_descricao_acao()}"
